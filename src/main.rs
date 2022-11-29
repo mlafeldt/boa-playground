319577d45a6c01bc0e62de1cfafd9fe4773aa112
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2022-11-29 23:21:17 +0100

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
-use boa_engine::{Context, JsValue};
+use boa_engine::property::Attribute;
+use boa_engine::{builtins::JsArgs, Context, JsResult, JsValue};
 
 fn main() {
-    let js_code = r#"console.log("Hello World from a JS code string!")"#;
+    let js_code = r#"
+        console.log("Hello World from a JS code string!");
+        say_hello(PROJECT_VERSION);
+        PROJECT_VERSION = "0.0.1";
+    "#;
 
     let mut ctx = Context::default();
 
-    let stmts = ctx.parse(js_code).unwrap();
-    let code_block = ctx.compile(&stmts).unwrap();
+    ctx.register_global_builtin_function("say_hello", 1, say_hello);
+    ctx.register_global_property("PROJECT_VERSION", "1.0.0", Attribute::READONLY);
 
-    match ctx.execute(code_block) {
+    // let stmts = ctx.parse(js_code).unwrap();
+    // let code_block = ctx.compile(&stmts).unwrap();
+    // ctx.execute(code_block.clone())
+
+    match ctx.eval(&js_code) {
         Ok(res) if !res.is_undefined() => {
             println!("{}", JsValue::to_json(&res, &mut ctx).unwrap());
         }
@@ -19,4 +28,16 @@ fn main() {
             eprintln!("Uncaught {}", e.display());
         }
     }
+}
+
+fn say_hello(_this: &JsValue, args: &[JsValue], ctx: &mut Context) -> JsResult<JsValue> {
+    let name = args.get_or_undefined(0);
+
+    if name.is_undefined() {
+        println!("Hello World!");
+    } else {
+        println!("Hello {}!", name.to_string(ctx)?);
+    }
+
+    Ok(JsValue::undefined())
 }
